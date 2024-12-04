0648050b62015379ebdc1747cdae4c5b7e1927e4
Duong Hiep <duonghiep416@gmail.com>
2024-12-04 22:16:09 +0700

    feat/navbar-ui

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,39 +1,12 @@
-import Image from 'next/image';
 import styles from './Header.module.scss';
 import clsx from 'clsx';
 import ThemeSwitch from '@/components/ThemeSwitch/ThemeSwitch';
-import { PiListBold } from 'react-icons/pi';
+import { Navbar } from '@/components/Navbar/Navbar';
 
 export const Header = () => {
   return (
     <header className={clsx(styles.header, 'dark:bg-neutral-dark-md')}>
-      <div className={clsx(styles.side)}>
-        <PiListBold className={clsx(styles.navIconMobile)} />
-        <Image
-          src="/logo-mobile.png"
-          width={42}
-          height={42}
-          alt="Brain Flip Logo"
-          className="max-h-9"
-        />
-        <ul className={clsx(styles.nav)}>
-          <li className={clsx(styles.navItem)}>
-            <a href="" className={clsx(styles.navLink)}>
-              Home
-            </a>
-          </li>
-          <li className={clsx(styles.navItem)}>
-            <a href="" className={clsx(styles.navLink)}>
-              Explore
-            </a>
-          </li>
-          <li className={clsx(styles.navItem)}>
-            <a href="" className={clsx(styles.navLink)}>
-              Exams
-            </a>
-          </li>
-        </ul>
-      </div>
+      <Navbar />
       <div className={clsx(styles.side)}>
         <ThemeSwitch />
       </div>
